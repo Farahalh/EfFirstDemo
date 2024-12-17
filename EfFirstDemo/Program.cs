5080936fa765cac7d46d88406f6cd1c3e823d66b
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2024-12-17 13:36:01 +0100

    Add initial implementation for EF Core demo application
    **Description:**
    - Introduced `Program` class to demonstrate basic CRUD operations using EF Core.
    - Implemented `DbDataGenerator` class to populate the database with sample data.
    - Configured database connection using a local SQLite file.
    - Ensured database is recreated from the model on each run using `EnsureDeleted` and `EnsureCreated`.
    - Generated database creation script for debugging purposes.
    - Demonstrated Create, Read, Update, and Delete operations with example entities (`Blog` and `Post`).
    
    This commit serves as a foundational demonstration of EF Core functionalities.

---
+++ EfFirstDemo/Program.cs
@@ -0,0 +1,76 @@
+namespace EfFirstDemo;
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    private static string GetConnectionString()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        return System.IO.Path.Join(path, "blogging.db");
+    }
+    static void Main(string[] args)
+    {
+        
+        var connectionString = Program.GetConnectionString();
+
+        //Create DbContext instance
+        using var db = new BloggingContext();
+
+        //Delete the database everytime
+        db.Database.EnsureDeleted();
+        db.Database.EnsureCreated();
+
+        //Recreate he database from the model
+        //this means you can change your model and the database will map your entities
+        var sqlenDb = db.Database.GenerateCreateScript();
+        Debug.WriteLine(sqlenDb);
+
+        //Just for fun
+        var dbGenerator = new DbDataGenerator();
+        dbGenerator.PopulateDb(db);
+    }
+}
+ 
+/// <summary>
+/// Responsible for populating the database with data
+/// </summary>
+public class DbDataGenerator
+{
+    public DbDataGenerator()
+    {
+
+    }
+
+    public void PopulateDb(BloggingContext db)
+    {
+        // Create
+        Console.WriteLine("Inserting a new blog");
+        db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+        db.Add(new Blog { Url = "http://microsoft.com" });
+        db.Add(new Blog { Url = "http://helloworld.com" });
+        db.SaveChanges();
+
+        // Read
+        Console.WriteLine("Querying for a blog");
+        var blog = db.Blogs
+            .OrderBy(b => b.BlogId)
+            .First();
+
+        // Update
+        Console.WriteLine("Updating the blog and adding a post");
+        blog.Url = "https://devblogs.microsoft.com/dotnet";
+        blog.Posts.Add(
+            new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+        db.SaveChanges();
+
+        // Delete
+        Console.WriteLine("Delete the blog");
+        db.Remove(blog);
+        db.SaveChanges();
+    }
+}
