520579c080309e5da1d6cd55f45a9a781a03a0d5
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2024-12-17 13:38:33 +0100

    Added migrations and updated Database

---
+++ EfFirstDemo/Migrations/20241217103307_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfFirstDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moo",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moo", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Raawr",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Raawr", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Raawr_Moo_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Moo",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Raawr_BlogId",
+                table: "Raawr",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Raawr");
+
+            migrationBuilder.DropTable(
+                name: "Moo");
+        }
+    }
+}
