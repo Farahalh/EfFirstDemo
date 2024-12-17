ffccb66d19e870e27afa8df6ccada25a5c1b33a0
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2024-12-17 13:33:48 +0100

    Commenting out EfFirstDemoTest bcs I switched from Sqlite to Sqlserver.

---
+++ EfFirstDemoTests/BloggingContextTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfFirstDemoTests
+{
+    //[TestClass()]
+    //public class BloggingContextTests
+    //{
+    //    [TestMethod()]
+    //    public void CanConstructBloggingContext()
+    //    {
+    //        //arrange
+    //        var expectedConnectionString = "BloggingTestDatabase.db";
+
+    //        //act
+    //        var dbContext = new BloggingContext(expectedConnectionString);
+    //        var actualConnectionString = dbContext.DbPath;
+
+    //        //assert
+    //        Assert.AreEqual(expectedConnectionString, actualConnectionString);
+    //    }
+    //}
+}
